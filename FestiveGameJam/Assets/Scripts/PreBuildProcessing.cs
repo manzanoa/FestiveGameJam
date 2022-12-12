82f031d8b198bd7d29aa76e0461587a3ebddc459
Adrian <36515623+manzanoa@users.noreply.github.com>
2022-12-12 16:56:05 -0500

    a

--- FestiveGameJam/Assets/Scripts/PreBuildProcessing.cs
+++ FestiveGameJam/Assets/Scripts/PreBuildProcessing.cs
@@ -9,7 +9,7 @@ public class PreBuildProcessing : IPreprocessBuildWithReport
     public int callbackOrder => 1;
     public void OnPreprocessBuild(BuildReport report)
     {
-        System.Environment.SetEnvironmentVariable("EMSDK_PYTHON", "/Library/Frameworks/Python.framework/Versions/2.7/bin/python");
+        System.Environment.SetEnvironmentVariable("EMSDK_PYTHON", "/Library/Frameworks/Python.framework/Versions/3.9/bin");
     }
 }
 #endif
